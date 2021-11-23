cff1c420e4956a1f6c0c509a26a8dbb13681abd6
PatricioOsorio <patriciomiguel_12@hotmail.com>
2021-11-23 14:14:39 -0600

    Primer commit

---
+++ scripts/dark_mode.js
@@ -0,0 +1,44 @@
+const d = document;
+const w = window;
+export default function darkMode() {
+  const $dataDark = d.querySelector(`[data-dark]`);
+  const $btnDarkmodeIcon = d.querySelector('.btn-darkmode__icon');
+  const $dynamicIcons = d.querySelectorAll('.dynamic-icon');
+
+  const localConfig = localStorage.getItem('theme');
+
+  const changeColorIcons = () => {
+    $dynamicIcons.forEach((element) => {
+      element.classList.toggle('filter-invert');
+    });
+  };
+
+  const lightMode = () => {
+    $dataDark.classList.remove('dark-mode');
+    $btnDarkmodeIcon.src = 'assets/icons/moon.svg';
+    localStorage.setItem('theme', 'light');
+    changeColorIcons();
+  };
+
+  const darkMode = () => {
+    $dataDark.classList.add('dark-mode');
+    $btnDarkmodeIcon.src = 'assets/icons/sun.svg';
+    localStorage.setItem('theme', 'dark');
+    changeColorIcons();
+  };
+
+  d.addEventListener('click', (e) => {
+    if (
+      e.target.matches('.btn-darkmode') ||
+      e.target.matches('.btn-darkmode *')
+    ) {
+      $dataDark.classList.contains('dark-mode') ? lightMode() : darkMode();
+    }
+  });
+
+  d.addEventListener('DOMContentLoaded', (e) => {
+    if (localConfig === null) localStorage.setItem('theme', 'light');
+    if (localConfig === 'light') lightMode();
+    if (localConfig === 'dark') darkMode();
+  });
+}
---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+import darkMode from './dark_mode.js';
+
+const d = document;
+darkMode();
+
+d.addEventListener('DOMContentLoaded', (e) => {});
